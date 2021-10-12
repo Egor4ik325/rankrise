6d5e424e12e76c344a55ca7d8492bea7fae250c6
Egor4ik325 <zorinegor325@gmail.com>
2021-10-12 19:51:27 +0300

    Connect PostgreSQL db

--- api/rankrise/settings.py
+++ api/rankrise/settings.py
@@ -12,18 +12,24 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+from django.core.management.utils import get_random_secret_key
+from environ import Env
+
+env = Env(DEBUG=(bool, True))
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+Env.read_env(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(eygy#^)cv-i$w2i-l@r=ph(u432z1qe^&$skq@k-!$9fc%)6m'
+SECRET_KEY = env("DJANGO_SECRET_KEY", default=get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = []
 
@@ -74,9 +80,13 @@ WSGI_APPLICATION = 'rankrise.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "USER": env("POSTGRES_USER", default="postgres"),
+        "PASSWORD": env("POSTGRES_PASSWORD", default="postgres"),
+        "NAME": env("POSTGRES_DB", default="postgres"),
+        "HOST": "db",
+        "PORT": "5432",
     }
 }
 
@@ -123,3 +133,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# AUTH_USER_MODEL = "authentication.CustomUser"
