9098ed88dd0c264d9cef84673512e8d892b2c9af
Egor4ik325 <zorinegor325@gmail.com>
2022-02-08 23:56:49 +0300

    Add not found page design

--- spa/src/pages/NotFound.js
+++ spa/src/pages/NotFound.js
@@ -1,5 +1,23 @@
+import notFoundIllustration from "../assets/img/illustrations/not-found.svg";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import routes from "../routes";
+
 const NotFound = () => {
-  return <>404: Page not found</>;
+  return (
+    <div className="not-found">
+      <img className="not-found-illustration" src={notFoundIllustration} />
+      <h1 className="not-found-title">Page not found</h1>
+      <Button
+        className="not-found-go-home"
+        variant="tertiary"
+        as={Link}
+        to={routes.home}
+      >
+        Go Home
+      </Button>
+    </div>
+  );
 };
 
 export default NotFound;
